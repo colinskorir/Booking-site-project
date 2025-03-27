98e2667cb10ada2894c904381505a951cdbdf27d
colinskorir <korircoll20@gmail.com>
2025-03-27 23:15:33 +0300

    added files

--- src/index.js
+++ src/index.js
@@ -1,2 +1,7 @@
 document.getElementById('bookingForm').addEventListener('submit', async function(e) {
     e.preventDefault();
+    
+    const departure = document.getElementById('departure').value;
+    const destination = document.getElementById('destination').value;
+    const date = document.getElementById('date').value;
+    
